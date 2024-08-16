bb1ec0dbd41da7c8f75c84c8ef6727458bd88889
bostxavier <xavierbost@orange.fr>
2024-08-16 12:27:12 +0200

    feat: complete exercise 3.7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const req = require('express/lib/request')
-const app = express()
+const morgan = require('morgan')
+morgan(':method :url :status :res[content-length] - :response-time ms')
 
+const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
