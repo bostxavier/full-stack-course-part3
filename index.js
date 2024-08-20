e1ce549842db24262e36e14b7a4878930f718792
bostxavier <xavierbost@orange.fr>
2024-08-20 10:37:50 +0200

    feat: complete exercise 3.13

--- index.js
+++ index.js
@@ -5,45 +5,29 @@ morgan.token('body', request => request.method === 'POST' || request.method === 
 
 const app = express()
 
+require('dotenv').config()
+const Person = require('./models/person')
+
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
     const datetime = new Date()
-    const resp = `
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${datetime}</p>
-    `
-    response.send(resp)
+    Person.find({}).then(persons => {
+        const resp = `
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${datetime}</p>
+        `
+        response.send(resp)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
